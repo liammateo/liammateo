7c2aa6c4d1a6f91d45e512c45218ab65b8e968f1
liammateo <109359388+liammateo@users.noreply.github.com>
2022-08-08 21:13:55 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,149 @@
+from tkinter.tix import Select
+from flask import Flask, redirect
+from flask import render_template,request,redirect, url_for
+from flaskext.mysql import MySQL
+from flask import send_from_directory
+from flask_login import LoginManager
+from datetime import datetime
+import os
+
+app= Flask(__name__)
+
+mysql=MySQL()
+app.config['MYSQL_DATABASE_HOST']='localhost'
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']='liammateo18'
+app.config['MYSQL_DATABASE_DB']='sistema'
+mysql.init_app(app)
+
+CARPETA=os.path.join('uploads')
+app.config['CARPETA']=CARPETA
+
+@app.route('/uploads/<nombreFoto>')
+def uploads(nombreFoto):
+    return send_from_directory(app.config['CARPETA'],nombreFoto)
+
+@app.route("/")
+def index():
+    sql="SELECT * FROM `empleados`;"#CONSULTA BASE DE DATOS
+    #conección a base de datos y envio de datos!
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql)
+    empleados=cursor.fetchall() #selecciona todolos datos.
+    print(empleados) #imprime en consola
+    conn.commit()
+    return render_template('empleados/index.html', empleados=empleados)
+
+@app.route("/signup/", methods=["GET", "POST"])
+def show_signup_form():
+    return render_template("login.html")
+
+@app.route('/destroy/<int:id>')#<int:id> recibe un entero!!!
+def destroy(id):
+    #Funcion para borrar un empleado a travez de un ID.
+    conn=mysql.connect()
+    cursor=conn.cursor()    
+    cursor.execute("SELECT foto FROM empleados WHERE id=%s", id)
+    fila=cursor.fetchall()
+    os.remove(os.path.join(app.config['CARPETA'],fila[0][0]))    
+    cursor.execute("DELETE FROM empleados WHERE id=%s",(id))
+    conn.commit()
+    return redirect('/') #redirecciona
+
+@app.route('/edit/<int:id>') #para editar usuarios
+def edit(id):
+    conn=mysql.connect() #conecta base de datos
+    cursor=conn.cursor() #almacena datos de consulta
+    cursor.execute("SELECT * FROM empleados WHERE id=%s", (id))
+    empleados=cursor.fetchall() #selecciona todolos datos.
+    conn.commit()
+    return render_template('empleados/edit.html', empleados=empleados)
+
+@app.route('/update', methods=['POST'])
+def update(): #Función para actualizar datos de un usuario con el EDIT
+    _nombre=request.form['txtNombre']
+    _correo=request.form['txtCorreo']
+    _tipoUser=request.form['txtTipoUsuario']
+    _foto=request.files['txtFoto']
+    id=request.form['txtID']
+    #aca agregamos a la BD lo que agregamos en el formulario
+    sql="UPDATE empleados SET nombre=%s, correo=%s, tipoUsuario=%s WHERE `empleados`.`id` =%s ;" #sin 'empleados' no funciona, tener en cuenta!!!
+    #introducción de datos
+    datos=(_nombre,_correo,_tipoUser,id)
+    #conección a base de datos y envio de datos!
+    conn=mysql.connect()
+    cursor=conn.cursor()    
+    now=datetime.now() #tiempo actual
+    tiempo=now.strftime("%Y%M%d%H%M%S")
+
+    #consultamos si hay foto mismo nombre, guardamos por fecha.
+    if _foto.filename!='':
+        nuevoNombreFoto=tiempo+_foto.filename
+        _foto.save("uploads/"+nuevoNombreFoto)        
+        cursor.execute("SELECT foto FROM empleados WHERE id=%s", id)
+        fila=cursor.fetchall()
+        os.remove(os.path.join(app.config['CARPETA'],fila[0][0]))
+        cursor.execute("UPDATE empleados SET foto=%s WHERE id=%s",(nuevoNombreFoto,id))
+        conn.commit()
+
+    cursor.execute(sql,datos)
+    conn.commit()
+    return redirect('/')
+
+@app.route('/create')
+def create():
+    return render_template('empleados/create.html')
+
+@app.route('/empl')
+def empl():
+    sql="SELECT * FROM `empleados`;"#CONSULTA BASE DE DATOS
+    #conección a base de datos y envio de datos!
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql)
+    empleados=cursor.fetchall() #selecciona todolos datos.
+    conn.commit()
+    return render_template('empleados/empl.html', empleados=empleados)
+
+@app.route('/clientes')
+def clientes():
+    sql="SELECT * FROM `empleados`;"#CONSULTA BASE DE DATOS
+    #conección a base de datos y envio de datos!
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql)
+    empleados=cursor.fetchall() #selecciona todolos datos.
+    conn.commit()
+    return render_template('empleados/clientes.html', empleados=empleados)
+    
+
+@app.route('/store', methods=['POST'])
+#para ingresar desde el formulario "create.html"
+def storage():
+    #variables
+    _nombre=request.form['txtNombre']
+    _correo=request.form['txtCorreo']
+    _tipoUser=request.form['txtTipoUsuario']
+    _foto=request.files['txtFoto']
+    #para evitar sobre escribir fotos con el mismo nombre
+    now=datetime.now() #tiempo actual
+    tiempo=now.strftime("%Y%M%d%H%M%S")
+    #consultamos si hay foto mismo nombre, guardamos por fecha.
+    if _foto.filename!='':
+        nuevoNombreFoto=tiempo+_foto.filename
+        _foto.save("uploads/"+nuevoNombreFoto)
+    #aca agregamos a la BD lo que agregamos en el formulario
+    sql="INSERT INTO `empleados` (`id`, `nombre`, `correo`, `tipoUsuario`, `foto`) VALUES (NULL, %s, %s, %s, %s) ;"
+    #introducción de datos
+    datos=(_nombre,_correo,_tipoUser,nuevoNombreFoto)
+    #conección a base de datos y envio de datos!
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql,datos)
+    conn.commit()
+    return redirect('/')
+    
+if __name__=='__main__':
+    app.run(debug=True)
+    
